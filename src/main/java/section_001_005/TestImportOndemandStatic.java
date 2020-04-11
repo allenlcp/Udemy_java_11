ca0e38891e308196ace956fd04a24530cfaee8e0
allenlcp <allen.mygithub@gmail.com>
2020-04-10 22:38:13 -0400

    added maven framework

--- src/main/java/section_001_005/TestImportOndemandStatic.java
+++ src/main/java/section_001_005/TestImportOndemandStatic.java
@@ -1,17 +1,20 @@
 package section_001_005;
 
-public class TestImportStatic {
+// A static-import on demand declaration for static variable/method
+import static java.lang.Math.*;
+
+public class TestImportOndemandStatic {
     public static void main(String[] args) {
         int radius = 2;
 
         // We can use a static class variable on the Math class to
         // get the value of PI
-        double circumference = 2 * java.lang.Math.PI * radius;
+        double circumference = 2 * PI * radius;
         System.out.println("circumference = " + circumference);
 
         // We can use a static class method on the Math class to
         // get the square root of a number
-        double sqrt81 = java.lang.Math.sqrt(81);
+        double sqrt81 = sqrt(81);
         System.out.println("sqrt81 = " + sqrt81);
     }
 }
---
+++ src/main/java/section_001_005/TestImportSingleStatic.java
@@ -0,0 +1,23 @@
+package section_001_005;
+
+// A single-static-import declaration for static variable
+import static java.lang.Math.PI;
+
+// A single-static-import declaration for static method
+import static java.lang.Math.sqrt;
+
+public class TestImportSingleStatic {
+    public static void main(String[] args) {
+        int radius = 2;
+
+        // We can use a static class variable on the Math class to
+        // get the value of PI
+        double circumference = 2 * PI * radius;
+        System.out.println("circumference = " + circumference);
+
+        // We can use a static class method on the Math class to
+        // get the square root of a number
+        double sqrt81 = sqrt(81);
+        System.out.println("sqrt81 = " + sqrt81);
+    }
+}
